e98f901d9add52bd39b0149e57b178c8ba0b9e48
Mehmet HARMAN <mehmet_harman@aof.anadolu.edu.tr>
2021-02-02 07:52:57 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // ientity implement eden class bir veritabnı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
