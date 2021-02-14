cec52f5e5d826ce50dcba4e31334edf667fb5f69
Mehmet HARMAN <mehmet_harman@aof.anadolu.edu.tr>
2021-02-14 03:50:12 +0300

    Day 9

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,15 +7,32 @@ namespace ConsoleUI
 {
     //SOLID
     // Open Closed principle
+    //IoC
+    //DTO data transformation object
     class Program
     {
         static void Main(string[] args)
         {
+            ProductTest(); //methoda aktardık
+            //CategoryTest(); // methoda aktardık
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
         }
     }
